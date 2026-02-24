327e6eafc0eee48de5723ba4ea3dad1169fe12af
Dragon Chan <dragonchan@dhcp-10-101-17-231.wireless.northwestern.private>
2026-02-23 18:53:14 -0600

    Revert routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
 
-  get("/places", { :controller => "places", :action => "index" })
-  get("/places/new", { :controller => "places", :action => "new" })
-  post("/places", { :controller => "places", :action => "create" })
-  get("/places/:id", { :controller => "places", :action => "show" })
-
-  get("/entries/new", { :controller => "entries", :action => "new" })
-  post("/entries", { :controller => "entries", :action => "create" })
+  resources :places
+  resources :entries
 end
